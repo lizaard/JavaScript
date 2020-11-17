8da0e67de4089fced69872555923a950dadf1de0
Lizard <41157961+lizaard@users.noreply.github.com>
2020-11-17 14:42:59 +0200

    ab

--- JS-pt1/script.js
+++ JS-pt1/script.js
@@ -33,7 +33,7 @@ console.log(andrei);
 
 
 
-const manualCart = function() {
+const manualCart = function () {
     let cart = document.querySelector(".cart");
     let heightCart = cart.offsetHeight;
     let widthCart = cart.offsetWidth;
@@ -48,10 +48,10 @@ const manualCart = function() {
     console.log(heightCard);
     console.log(widthCard);
 
-    if ( widthCart == widthCard && heightCart == heightCard ) {
+    if (widthCart == widthCard && heightCart == heightCard) {
         cart.innerHTML = 'Andrei e smecher'
         card.innerHTML = 'Andrei e smecher'
-        
+
     } else {
         cart.innerHTML = 'Andrei e bou'
         card.innerHTML = 'Andrei e bou'
@@ -59,42 +59,42 @@ const manualCart = function() {
 }; manualCart();
 
 
-const codingChallengeThere = function() {
-    
-let theDolphinsAverageScore = (97 + 112 + 101) / 3;
-let theKoalasAverageScore = (109 + 95 + 123) / 3;
+const codingChallengeThere = function () {
 
-console.log(theDolphinsAverageScore);
-console.log(theKoalasAverageScore);
+    let theDolphinsAverageScore = (97 + 112 + 101) / 3;
+    let theKoalasAverageScore = (109 + 95 + 123) / 3;
 
-if (theDolphinsAverageScore > theKoalasAverageScore && theDolphinsAverageScore >= 100) {
-    console.log('The Dolphins are the best');
-} else if (theKoalasAverageScore > theDolphinsAverageScore && theKoalasAverageScore >= 100) {
-    console.log('The koala are the best');
-} else if (theDolphinsAverageScore === theKoalasAverageScore) {
-    console.log('There is a draw!!');
-} else {
-    console.log('THe game was no Contest');
-}
+    console.log(theDolphinsAverageScore);
+    console.log(theKoalasAverageScore);
+
+    if (theDolphinsAverageScore > theKoalasAverageScore && theDolphinsAverageScore >= 100) {
+        console.log('The Dolphins are the best');
+    } else if (theKoalasAverageScore > theDolphinsAverageScore && theKoalasAverageScore >= 100) {
+        console.log('The koala are the best');
+    } else if (theDolphinsAverageScore === theKoalasAverageScore) {
+        console.log('There is a draw!!');
+    } else {
+        console.log('THe game was no Contest');
+    }
 
 
-}; 
+};
 codingChallengeThere();
 
 
 
 
 
-const daySwitch = function() {
+const daySwitch = function () {
     const day = 'monday';
     const dayli = 'moon';
 
     switch (day, dayli) {
         case "monday": console.log('TEST!');
-                       console.log('TEST2');
+            console.log('TEST2');
             break;
-    case "moon": console.log('merge');
-    break;
+        case "moon": console.log('merge');
+            break;
         default: console.log('test1');
             break;
     }
@@ -103,7 +103,7 @@ daySwitch();
 
 
 
-const ternaryOperator = function() {
+const ternaryOperator = function () {
     const age = 27;
     age >= 18 ? console.log("bea bere") : console.log('bea apa');;
 
@@ -116,25 +116,25 @@ ternaryOperator();
 
 
 
-const codingChallengeFour = function() {
+const codingChallengeFour = function () {
 
     function methodOne() {
         const billOne = 275;
         const billTwo = 40;
         const billTree = 430;
-    
+
         const tipBillOne = (billOne / 100) * 15;
         console.log(tipBillOne);
         const tipBillTwo = (billTwo / 100) * 20;
         console.log(tipBillTwo);
         const tipBillTree = (billTree / 100) * 20;
         console.log(tipBillTree);
-    
+
         console.log(`The bill was ${billOne}, the tip was ${tipBillOne}, and the total value ${billOne + tipBillOne}`);
         console.log(`The bill was ${billTwo}, the tip was ${tipBillTwo}, and the total value ${billTwo + tipBillTwo}`);
-        console.log(`The bill was ${billTree}, the tip was ${tipBillTree}, and the total value ${billTree + tipBillTree}`);        
+        console.log(`The bill was ${billTree}, the tip was ${tipBillTree}, and the total value ${billTree + tipBillTree}`);
     } methodOne();
-    
+
     function methodTwo() {
         const BillOne = 275;
         const BillTwo = 40;
@@ -153,20 +153,20 @@ const codingChallengeFour = function() {
 codingChallengeFour();
 
 
-const forWhileEcample = function() {
+const forWhileEcample = function () {
 
     let suma = 0;
 
     for (let index = 0; index <= 100; index++) {
-        suma = suma + index; 
-        
+        suma = suma + index;
+
     }
 
     console.log(suma);
     var index = 0;
-    while (index < 10) { 
+    while (index < 10) {
         index++;
-        console.log(index);   
+        console.log(index);
     }
 
 
@@ -180,31 +180,31 @@ const forWhileEcample = function() {
     //     } else {
     //         console.log('michele');
     //     }
-        
+
     // }
 
-   
-    
-    
+
+
+
 };
- forWhileEcample();
+forWhileEcample();
 
 
- function whiiilee() {
+function whiiilee() {
     var i = 0;
-    while(i < 16) {
+    while (i < 16) {
         i++
         if (i % 4 == 0) {
             console.log('diana');
         } else if (i % 4 == 1) {
             console.log('andrei');
-        } else if ( i % 4 == 2) {
+        } else if (i % 4 == 2) {
             console.log('mihaela');
         } else {
             console.log('michele');
         }
     }
-    
+
 }
 whiiilee();
 
@@ -215,13 +215,13 @@ var xxx = function CalculateAge(birthYear, currentYear) {
 
 
 
-var ANIDIANEI = function() {
+var ANIDIANEI = function () {
     var yyy = xxx;
     (function () {
-            let year = new Date().getFullYear();
-            console.log(year);
-            var calculatorcacat = yyy(1996, year)
-            console.log(calculatorcacat);
+        let year = new Date().getFullYear();
+        console.log(year);
+        var calculatorcacat = yyy(1996, year)
+        console.log(calculatorcacat);
     })();
 }
 ANIDIANEI();
@@ -232,8 +232,8 @@ const Pensie = (e) => {
     const age = 2020 - e;
     const ret = 65 - age;
     return ret;
-    
-    
+
+
 }
 console.log(Pensie(1993));
 
@@ -242,39 +242,39 @@ console.log(Pensie(1993));
 
 
 
-let codingChallengeFive = (function() {
-        const calcAverageScore = (scoreOne, scoreTwo, scoreThree) => {
-            return (scoreOne + scoreTwo + scoreThree) / 3;
-        }
-        const dolphinsAvrgScore = calcAverageScore(44, 23, 71);
-        const dolphinsAvrgScoreTwo = calcAverageScore(85, 54, 41);
-        const koalasAvrgScore = calcAverageScore(65, 54, 49);
-        const koalasAvrgScoreTwo = calcAverageScore(23, 34, 27);
-        console.log(dolphinsAvrgScore, koalasAvrgScore);
-        console.log(dolphinsAvrgScoreTwo, koalasAvrgScoreTwo);
+let codingChallengeFive = (function () {
+    const calcAverageScore = (scoreOne, scoreTwo, scoreThree) => {
+        return (scoreOne + scoreTwo + scoreThree) / 3;
+    }
+    const dolphinsAvrgScore = calcAverageScore(44, 23, 71);
+    const dolphinsAvrgScoreTwo = calcAverageScore(85, 54, 41);
+    const koalasAvrgScore = calcAverageScore(65, 54, 49);
+    const koalasAvrgScoreTwo = calcAverageScore(23, 34, 27);
+    console.log(dolphinsAvrgScore, koalasAvrgScore);
+    console.log(dolphinsAvrgScoreTwo, koalasAvrgScoreTwo);
 
-        function CheckWinner(dAvrg, kAvrg) {
-            if (dAvrg > kAvrg) {
-                return `Dolphins win ${dAvrg} vs. ${kAvrg}`
-            } else {
-                return `Koala win ${kAvrg} vs. ${dAvrg}`
-            }
+    function CheckWinner(dAvrg, kAvrg) {
+        if (dAvrg > kAvrg) {
+            return `Dolphins win ${dAvrg} vs. ${kAvrg}`
+        } else {
+            return `Koala win ${kAvrg} vs. ${dAvrg}`
         }
-        console.log(CheckWinner(dolphinsAvrgScore, koalasAvrgScore));
-        console.log(CheckWinner(dolphinsAvrgScoreTwo, koalasAvrgScoreTwo));
+    }
+    console.log(CheckWinner(dolphinsAvrgScore, koalasAvrgScore));
+    console.log(CheckWinner(dolphinsAvrgScoreTwo, koalasAvrgScoreTwo));
 
 })();
 
 
 
 
-let AnaidExercitiu = (function() {
+let AnaidExercitiu = (function () {
     for (let index = 1; index <= 50; index++) {
-        if (index % 3 == 0 && index % 5 == 0 ) {
+        if (index % 3 == 0 && index % 5 == 0) {
             console.log('fizzbuzz');
-        } else if(index % 5 == 0) {
+        } else if (index % 5 == 0) {
             console.log('fizz');
-        } else if(index % 3 == 0) {
+        } else if (index % 3 == 0) {
             console.log('buzz');
         } else {
             console.log(index);
@@ -285,7 +285,7 @@ let AnaidExercitiu = (function() {
 
 
 
-const andreiArray = new Array(12,13,14,15)
+const andreiArray = new Array(12, 13, 14, 15)
 console.log(andreiArray[andreiArray.length - 1]);
 
 
@@ -298,52 +298,87 @@ let Calculu = (() => {
 
     egal = [];
     console.log(first, second, egal);
-   
-    function SumaNumberelor(a,b) {
+
+    function SumaNumberelor(a, b) {
         return a + b;
     }
 
     function AdaugaSuma() {
         let sumar = SumaNumberelor(first, second)
         console.log(sumar);
-    }AdaugaSuma()
+    } AdaugaSuma()
 
     egal.innerHTML = AdaugaSuma();
     console.log(egal);
-    let rezultat = button.addEventListener('click', function() {
-       
+    let rezultat = button.addEventListener('click', function () {
+
     })
-        console.log(rezultat);
-        
+    console.log(rezultat);
+
 
 })();
 
 var a = {}
 console.log(a);
 
-let m = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17];
-console.log(m[10] + 1, m[10 + 1]);
-for(let i = 0; i < m.length - 1; i++) {
-if(m[i] + 1 != m[i + 1]){
-    console.log(m[i] + 1);
-}
-  
+let m = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15];
+// console.log(m[10] + 1, m[10 + 1]);
+
+
+for (let i = 0; i < m.length - 1; i++) {
+    // console.log(m);
+    if (m[i] + 1 != m[i + 1]) {
+        console.log(m[i] + 1);
+    }
+
 }
 
 
 // for (let index = 0; index < m.length; index++) {
 //     if (condition) {
-        
+
 //     }
-    
+
 //     const element = m[index];
 //     console.log(element);
 // }
 // console.log(m.reduce(OAdunare));
 // let element= "0";
 // for (let index = 1; index <= 15; index++) {
-    
+
 //     element = element + index;
-    
+
 // }
 // console.log(element);
+
+
+
+// var Deloitte = (() =>{
+//         let index = [1,2,3,5]
+
+//         for (index = 1; index < 5; index++) {
+//             const index = index[index];
+//             console.log(index);
+//         }
+// });
+// Deloitte()
+
+
+let CalculezDinNou = (() => {
+    let testData = [125, 555, 44];
+    let Tips = [];
+
+    for (let i = 0; i <= testData.length - 1; i++) {
+        let bill = testData[i];
+        let z = function name() {
+            if (bill >= 50 && bill <= 300) {
+                Tips.push(bill * 0.15);
+            } else {
+                Tips.push(bill * 0.2);
+            }
+        }; z();
+    }
+    console.log(testData,Tips);
+
+
+}); CalculezDinNou()
